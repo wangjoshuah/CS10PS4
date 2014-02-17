2d7fc067fb870ad6d7f13153c17095216d205741
alextsu <alextsu@gmail.com>
2014-02-16 20:24:46 -0500

    main method

---
+++ src/HuffmanMain.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+
+public class HuffmanMain {
+
+	public Map<Character, Integer> ftable; 
+	public BufferedBitReader bitInput;
+	
+	public void FrequencyTable(String compressedPathName) throws IOException {
+		bitInput = new BufferedBitReader(compressedPathName);
+		while(bitInput.readBit() != -1) {
+			if(ftable.containsKey(key)) {
+				int temp = ftable.get(key) + 1;
+				ftable.put(key, temp);
+			}
+			else {
+				
+			}
+		}
+		
+		bitInput.close();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
