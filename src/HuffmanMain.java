08b904cfd17140e0b221606ee993db2f5381edcd
wangjoshuah <imac@dhcp-210-80.lab.cs.dartmouth.edu>
2014-02-18 21:29:32 -0500

    Create Tree
    implemented a method to create a tree from singleton trees. Also added
    in comments for my code

--- src/HuffmanMain.java
+++ src/HuffmanMain.java
@@ -23,15 +23,36 @@ public class HuffmanMain {
 		return ftable;
 	}
 
+	/**
+	 * Creates a priority queue filled with singleton leaf trees of the data from the frequency table we receive
+	 * @param table		frequency table mapping character values to their frequencies
+	 * @return
+	 */
 	public PriorityQueue<CharacterTree> singletonsToPriorityQueue(Map<Character, Integer> table) {
-		PriorityQueue<CharacterTree> singletonQueue = new PriorityQueue<CharacterTree>();
-		Set<Character> charSet = table.keySet();
-		for (Character letter : charSet) {
-			UniChar data = new UniChar(letter.charValue(), table.get(letter).intValue());
-			CharacterTree singletonTree = new CharacterTree(data);
-			singletonQueue.add(singletonTree);
+		PriorityQueue<CharacterTree> singletonQueue = new PriorityQueue<CharacterTree>(); //create a priority queue to hold all the singleton trees
+		Set<Character> charSet = table.keySet(); //get the set of values for the frequency table
+		for (Character letter : charSet) { //for each key in the map,
+			UniChar data = new UniChar(letter.charValue(), table.get(letter).intValue()); //create data with the letter value and frequency
+			CharacterTree singletonTree = new CharacterTree(data); //create a leaf node to hold that data
+			singletonQueue.add(singletonTree); //add the leaf node to the priority queue
 		}
-		return singletonQueue;
+		return singletonQueue; //return our priority queue
+	}
+	
+	/**
+	 * Returns a fully constructed tree that will be our character binary tree mapping to binary bits
+	 * @param queueOfTrees 		priority queue of singleton trees with data
+	 * @return
+	 */
+	public CharacterTree createTree(PriorityQueue<CharacterTree> queueOfTrees) {
+		while (queueOfTrees.size() > 1) { //while we have multiple trees in our priority queue
+			CharacterTree lowestTree = queueOfTrees.remove(); //take the tree with the lowest frequency from the queue
+			CharacterTree secondLowestTree = queueOfTrees.remove(); //also take the second lowest tree from the queue
+			UniChar parentData = new UniChar(lowestTree.data.getFrequency(), secondLowestTree.data.getFrequency()); //create new data--the total frequency from the two lower nodes' data
+			CharacterTree parentTree = new CharacterTree(parentData, lowestTree, secondLowestTree); //create a parent tree holding the two lower trees
+			queueOfTrees.add(parentTree); //add the parent back into the priority queue to keep creating this tree
+		}
+		return queueOfTrees.remove(); //return the last tree remaining
 	}
 
 	public static void main(String[] args) {
