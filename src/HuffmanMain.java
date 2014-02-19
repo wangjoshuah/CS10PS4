5fd7f316663595963a87e24b182cc679fb545583
wangjoshuah <imac@dhcp-210-80.lab.cs.dartmouth.edu>
2014-02-18 23:33:55 -0500

    compression
    it works!

--- src/HuffmanMain.java
+++ src/HuffmanMain.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeMap;
 
+import javax.sound.sampled.Line;
 import javax.swing.JFileChooser;
 
 public class HuffmanMain {
 
-	public Map<Character, Integer> FrequencyTable(String text) {
+	private static Map<Character, Integer> frequencyTable(String text) {
 		Map<Character, Integer> ftable = new TreeMap<Character, Integer>(); //Create new Map that has a key as a character and an integer representing the count a character appears as its value
 		for (int i = 0; i < text.length(); i++) { //loop over each character in the string
 			char getChar = text.charAt(i); //get the character at a given i
@@ -28,7 +32,7 @@ public class HuffmanMain {
 	 * @param table		frequency table mapping character values to their frequencies
 	 * @return
 	 */
-	public PriorityQueue<CharacterTree> singletonsToPriorityQueue(Map<Character, Integer> table) {
+	private static PriorityQueue<CharacterTree> singletonsToPriorityQueue(Map<Character, Integer> table) {
 		PriorityQueue<CharacterTree> singletonQueue = new PriorityQueue<CharacterTree>(); //create a priority queue to hold all the singleton trees
 		Set<Character> charSet = table.keySet(); //get the set of values for the frequency table
 		for (Character letter : charSet) { //for each key in the map,
@@ -44,7 +48,7 @@ public class HuffmanMain {
 	 * @param queueOfTrees 		priority queue of singleton trees with data
 	 * @return
 	 */
-	public CharacterTree createTree(PriorityQueue<CharacterTree> queueOfTrees) {
+	private static CharacterTree createTree(PriorityQueue<CharacterTree> queueOfTrees) {
 		while (queueOfTrees.size() > 1) { //while we have multiple trees in our priority queue
 			CharacterTree lowestTree = queueOfTrees.remove(); //take the tree with the lowest frequency from the queue
 			CharacterTree secondLowestTree = queueOfTrees.remove(); //also take the second lowest tree from the queue
@@ -60,18 +64,22 @@ public class HuffmanMain {
 	 * @param codeTree		the binary tree with unique paths to each character
 	 * @return
 	 */
-	public Map<Character, String> codeRetreival(CharacterTree codeTree) {
+	private static Map<Character, String> codeRetreival(CharacterTree codeTree) {
 		CharacterTree pathHolder = codeTree; //create a path holder to remember where we are in the tree
 		String pathBitString = new String(); //have a string that keeps our path in terms of bits
 		Map<Character, String> codeMap = new TreeMap<Character, String>(); //instantiate the map we will return
 		while (!pathHolder.isLeaf()) { //while our current path still has children, 
 			if (pathHolder.getRight().isLeaf()) { //if our pathHolder's right child is a leaf,
+				System.out.println(pathBitString + "1");
 				codeMap.put(pathHolder.getRight().data.getKeyCharacter(), pathBitString + "1"); //add the right child's keycharacter and the current path concatenated with "1" since this child is on the right
+				System.out.println(codeMap.get(pathHolder.getRight().data.getKeyCharacter()));
 				pathHolder = (CharacterTree) pathHolder.getLeft(); //direct the path we keep going on to the left
 				pathBitString += "0"; //add "0" to our path bit string since we are going to the left for our next iteration of this loop
 			}
 			else { //our path continues down the right so the left one is the charcter and the right is more path
+				System.out.println(pathBitString + "0");
 				codeMap.put(pathHolder.getLeft().data.getKeyCharacter(), pathBitString + "0"); //add the left child and the path + "0" (do everything on the other side)
+				System.out.println(codeMap.get(pathHolder.getLeft().data.getKeyCharacter()));
 				pathHolder = (CharacterTree) pathHolder.getRight(); //direct the path to the right since the right is not a leaf
 				pathBitString += "1"; //add 1 to the path bit string since we are going to the right
 			}
@@ -79,6 +87,18 @@ public class HuffmanMain {
 		return codeMap; //return our map
 	}
 
+	private static Map<Character, String> codeRetreivalRecurs(CharacterTree codeTree, String bitString, Map<Character, String> codeMap) {
+		if (codeTree.isLeaf()) {
+			codeMap.put(codeTree.data.getKeyCharacter(), bitString);
+			System.out.println("Character is " + codeTree.data.getKeyCharacter() + " bit string is " + bitString);
+		}
+		else {
+			codeMap = codeRetreivalRecurs((CharacterTree) codeTree.getLeft(), bitString + "0", codeMap);
+			codeMap = codeRetreivalRecurs((CharacterTree) codeTree.getRight(), bitString + "1", codeMap);
+		}
+		return codeMap;
+	}
+	
 	public static String getFilePath() {
 		JFileChooser fc = new JFileChooser("."); // start at current directory
 
@@ -92,9 +112,35 @@ public class HuffmanMain {
 			return "";
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void compress(String filename) throws Exception{ //just let us work on this file please
+		BufferedReader in = new BufferedReader(new FileReader(filename)); //create a buffered reader to take in the file
+		String compressedFilename = filename.concat("compressed"); //add compressed for the compressed file's filename
+		BufferedBitWriter bitOutput = new BufferedBitWriter(compressedFilename); //create a new bitwrite to write a file here...
+		String fileText = new String(); //allocate a string to hold the file's text
+		String line;
+		while ((line = in.readLine()) != null) {
+			fileText += line; //get the string representation of the file
+		}
+		System.out.println(fileText);
+		in.close(); //close the file cuz we have what we need
+		Map<Character, String> characterMap = new TreeMap<Character, String>(); 
+		characterMap = codeRetreivalRecurs(createTree(singletonsToPriorityQueue(frequencyTable(fileText))), "", characterMap); //call our methods to get a frequency table then make a priority queue and then create a tree of those and then get the map code of that
+		for (int i = 0; i < fileText.length(); i ++) { //for the entire filestring,
+			String bitString = characterMap.get(fileText.charAt(i)); //get the bitstring of the character we are at
+			for (int j = 0; j < bitString.length(); j ++) { //for the entire bitstring
+				if (bitString.charAt(j) == '0') { //if the character is a 0,
+					bitOutput.writeBit(0); //write a 0 there
+				}
+				else { //if the character was a 1
+					bitOutput.writeBit(1); //write a 1 there
+				}
+			}
+		}
+	}
 
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		compress("inputs/USConstitution.txt");
 
 
 		/*
